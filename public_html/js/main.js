f94a11e91fea6df14ebe0d3a9729257931ffadeb
Marko Petronijevic <markobank87@gmail.com git config --global user.name Bankela>
2020-08-14 16:57:55 +0200

    inicijalizacija projekta

---
+++ public_html/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+//    let num=$(".counter").val();
+//      console.log(num);
+ $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+ 
+//   if($('.slider').length > 0){
+//        $(".slider").owlCarousel({
+//            
+//      items:4 
+//        });
+//    }
+// 
+    
+});
+
